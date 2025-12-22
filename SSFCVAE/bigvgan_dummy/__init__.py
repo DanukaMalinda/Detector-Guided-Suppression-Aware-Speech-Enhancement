824be5c5e52e84d13e7d44e08a7f54f2a0b903e1
DanukaMalinda <danuka.m@siu.edu>
2025-12-22 15:45:52 -0600

    SSFCVAE

---
+++ SSFCVAE/bigvgan_dummy/__init__.py
@@ -0,0 +1,38 @@
+from bigvgan.bigvgan import BigVGAN as Generator
+import bigvgan.bigvgan as bigvgan
+import json
+import os
+import torch
+
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+        
+
+def load_checkpoint(filepath):
+    assert os.path.isfile(filepath)
+    print("Loading '{}'".format(filepath))
+    checkpoint_dict = torch.load(filepath, map_location='cpu')
+    print("Complete.")
+    return checkpoint_dict
+
+
+def get_vocoder(model="bigvgan_22khz_80band"):
+    with open(f"bigvgan/configs/{model}.json") as f:
+        data = f.read()
+        
+    json_config = json.loads(data)
+    h = AttrDict(json_config)
+    
+    torch.manual_seed(h.seed)
+    torch.cuda.manual_seed(h.seed)
+
+    generator = Generator(h)
+    # state_dict_g = load_checkpoint(f"bigvgan/pretrained_models/{model}/g_05000000")
+    generator = bigvgan.BigVGAN.from_pretrained(f'nvidia/{model}', use_cuda_kernel=False)
+    # generator.load_state_dict(state_dict_g['generator'])
+    generator.eval()
+    generator.remove_weight_norm()
+    
+    return generator
